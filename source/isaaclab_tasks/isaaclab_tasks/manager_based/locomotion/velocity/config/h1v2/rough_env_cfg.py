78f057fec91bda3aff1d77e942f70a2fd719dd54
Alessandro Trovatello <atrovatell@iodake>
2025-02-28 14:28:33 +0100

    fine tuning training parameters

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/h1v2/rough_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/h1v2/rough_env_cfg.py
@@ -114,7 +114,7 @@ class H1v2RoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         ]
 
         # Rewards
-        self.rewards.undesired_contacts.weight = None #None
+        self.rewards.undesired_contacts = None #None
         self.rewards.flat_orientation_l2.weight = -1.0 #-1.0
         self.rewards.dof_torques_l2.weight = -0.01 #0.0 
         self.rewards.action_rate_l2.weight = -0.005 #-0.005
