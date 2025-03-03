eaa8cf7296b8f47e7fb7b8b7219b3ba2ea85945b
Alessandro Trovatello <atrovatell@iodake>
2025-03-03 14:20:44 +0100

    fine tuning training parameters

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/h1v2/rough_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/h1v2/rough_env_cfg.py
@@ -20,7 +20,7 @@ from isaaclab_assets import H1v2_MINIMAL_CFG  # isort: skip
 class H1v2Rewards(RewardsCfg):
     """Reward terms for the MDP."""
 
-    termination_penalty = RewTerm(func=mdp.is_terminated, weight=-200.0)
+    termination_penalty = RewTerm(func=mdp.is_terminated, weight=-400.0)
     lin_vel_z_l2 = None
     track_lin_vel_xy_exp = RewTerm(
         func=mdp.track_lin_vel_xy_yaw_frame_exp,
@@ -41,7 +41,7 @@ class H1v2Rewards(RewardsCfg):
     )
     feet_slide = RewTerm(
         func=mdp.feet_slide,
-        weight=-0.5, #-0.25
+        weight=-0.2, #-0.25
         params={
             "sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*ankle_roll_link"),
             "asset_cfg": SceneEntityCfg("robot", body_names=".*ankle_roll_link"),
@@ -59,7 +59,7 @@ class H1v2Rewards(RewardsCfg):
     )
     joint_deviation_arms = RewTerm(
         func=mdp.joint_deviation_l1,
-        weight=-0.3, # -0.2
+        weight=-0.2, # -0.2
         params={"asset_cfg": SceneEntityCfg("robot", joint_names=[".*_shoulder_.*_joint", ".*_elbow_joint", ".*_wrist_.*_joint"])}, # wrist added
     )
     joint_deviation_torso = RewTerm(
