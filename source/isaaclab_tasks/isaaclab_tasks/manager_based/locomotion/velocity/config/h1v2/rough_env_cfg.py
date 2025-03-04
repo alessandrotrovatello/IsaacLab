adc3bbd2f759d752f1f04d6ec21eae1de67b950c
Alessandro Trovatello <atrovatell@iodake>
2025-03-04 16:29:20 +0100

    fine tuning training parameters

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/h1v2/rough_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/h1v2/rough_env_cfg.py
@@ -36,7 +36,7 @@ class H1v2Rewards(RewardsCfg):
         params={
             "command_name": "base_velocity",
             "sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*ankle_roll_link"),
-            #"threshold": 0.05, #4
+            "threshold": 4.0, #4
         },
     )
     feet_slide = RewTerm(
@@ -121,7 +121,7 @@ class H1v2RoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         self.rewards.dof_acc_l2.weight = -1.25e-7 #-1.25e-7
 
         # Commands
-        self.commands.base_velocity.ranges.lin_vel_x = (0.0, 1.0)
+        self.commands.base_velocity.ranges.lin_vel_x = (0.0, 0.5)
         self.commands.base_velocity.ranges.lin_vel_y = (0.0, 0.0)
         self.commands.base_velocity.ranges.ang_vel_z = (-1.0, 1.0)
 
